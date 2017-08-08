e81048a02ba17a48ae80d2cb62ba4b59cca2f5ef
Sien <sienna2169@cmis.ac.th>
2017-08-08 17:29:58 +0700

    I hope this works!

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # APCS-sienna2169-cmis
 Sienna's repository for AP Computer Science.
 
-This class is gonna be great!
+Everything's working!
 
---
+++ Testing Stuff/Info.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Info here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Info
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Info
+     */
+    public Info()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
