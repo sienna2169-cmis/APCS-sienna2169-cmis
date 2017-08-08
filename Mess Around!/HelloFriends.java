0b9e4a13913371bb750a43e5a1e94400af616a16
cmis <cmis@cmis-hs07.(none)>
2017-08-08 11:53:05 +0700

    More practice

--- Mess Around!/HelloFriends.java
+++ Mess Around!/HelloFriends.java
@@ -3,7 +3,7 @@ public class HelloFriends
     public static void main ( String args[] )
         {
         System.out.println( "Hey guys! All my friends! How are you!!" );
-        
+        System.out.println( "I love social interaction!" );
         } // and that's where it ends folks
     
     
