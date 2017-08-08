843426b3e80ce9b47eb6ca2637ee2b8af09ffe4c
cmis <cmis@cmis-hs07.(none)>
2017-08-08 11:50:27 +0700

    Messed around :)

---
+++ Mess Around!/HelloFriends.java
@@ -0,0 +1,10 @@
+public class HelloFriends
+    {
+    public static void main ( String args[] )
+        {
+        System.out.println( "Hey guys! All my friends! How are you!!" );
+        
+        } // and that's where it ends folks
+    
+    
+   } // end of class HelloFriends
