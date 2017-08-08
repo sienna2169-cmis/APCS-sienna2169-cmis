a4eab087183da4deb47a1967ad58138c676dc053
cmis <cmis@cmis-hs07.(none)>
2017-08-08 11:39:56 +0700

    Created HelloWorld in FirstProject

---
+++ FirstProject/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+    {
+    public static void main ( String args[] )
+        {
+        System.out.println( "Hello everyone out there! ");   
+            
+        } // end method main
+        
+        
+    } // end class HelloWorld
