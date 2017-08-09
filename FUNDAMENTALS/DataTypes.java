5e73639a9aad1a2695d5e57aaf2be83df41f44b7
cmis <cmis@cmis-hs08.(none)>
2017-08-09 14:37:06 +0700

    Worked on Fundamentals

---
+++ FUNDAMENTALS/DataTypes.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class DataTypes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DataTypes
+{
+  public static void DataTypes() {
+      
+    }
+}
---
+++ FUNDAMENTALS1/Fundamentals.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class DataTypes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fundamentals
+{
+  public static void main(String []args) {
+    
+    
+    }
+  public static void dataTypes() {
+     byte a = 20;
+     System.out.println(a); 
+  
+     short b = 25000; 
+     System.out.println(b); 
+    }
+    
+}
+      
+    
+
