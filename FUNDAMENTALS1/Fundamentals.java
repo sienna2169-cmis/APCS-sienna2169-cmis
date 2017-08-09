70de8de763aeed8c47ae36c32016271cdf42b14a
Sien <sienna2169@cmis.ac.th>
2017-08-09 19:57:45 +0700

    Worked on Fundamentals problem 1

--- FUNDAMENTALS1/Fundamentals.java
+++ FUNDAMENTALS1/Fundamentals.java
@@ -7,16 +7,37 @@
  */
 public class Fundamentals
 {
-  public static void main(String []args) {
-    
-    
-    }
   public static void dataTypes() {
      byte a = 20;
      System.out.println(a); 
   
      short b = 25000; 
      System.out.println(b); 
+     
+     int c = 500;
+     System.out.println(c);
+     
+     long d = 5000000L;
+     System.out.println(d);
+     
+     float e = 345.87f;
+     System.out.println(e);
+     
+     double f = 56.7;
+     System.out.println(f);
+     
+     char g = 'S';
+     System.out.println(g);
+     
+     boolean h = true;
+     System.out.println(h);
+     
+     String bytetext = 
+     "A byte is a data type, usually used in place of integers, that is used to save space in large arrays. It can be used to represent numbers, such as prices and quantities."; 
+     
+ 
+     System.out.println(bytetext);
+     
     }
     
 }
--- Testing Stuff/Info.java
+++ Testing Stuff/Info.java
@@ -1,33 +1,17 @@
-
-/**
- * Write a description of class Info here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Info
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Info
-     */
-    public Info()
     {
-        // initialise instance variables
-        x = 0;
+    public static void main (String args[])
+        { 
+        System.out.println( "Everything is working great!" ) ;
+        
+        } // end method main
+    public static void main (Integer args[])
+        {
+        System.out.println(12345) ;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+    
+    
+    } // end class Info
+            
+         
+           
