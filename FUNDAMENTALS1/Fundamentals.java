b99a66d8bc8bc45d3c160e725512b36debaeb5fe
Sienna <sienna2169@cmis.ac.th>
2017-08-11 11:22:17 +0700

    Finishing up problem 1

--- FUNDAMENTALS1/Fundamentals.java
+++ FUNDAMENTALS1/Fundamentals.java
@@ -55,7 +55,38 @@ public class Fundamentals
      
      System.out.println(inttext);
      
-     String longtext = "";
+     String longtext = "A long is a datatype used in a similar way to integer, byte, and short data.\n" 
+     + "A long can be used to represent numbers and quantities.\n"
+     + "Since a long has a wider range than int, it is often used in place of integers when a wider range is needed.\n"
+     + "This means that long can be used in streams of data, lists, and other numerical data that usually would be represented by integers.\n";
+     
+     System.out.println(longtext);
+     
+     String floattext = "A float is a data type that is different from all the previous ones.\n"
+     + "A float deals with numerical data including floating point numbers.\n" 
+     + "However, a float is never used for precise values, i.e. currency. Therefore they are used for other sets of data that have decimal numbers.\n"
+     + "Some examples of this are averages, converted fractions, and lists of decimal numbers.\n";
+     
+     System.out.println(floattext);
+     
+     String doubletext = "A double is a data type that is similar to a float.\n"
+     + "Double is the default when it comes to decimal values. It is not used for precise values either, however.\n"
+     + "Doubles can be used for any numerical data consisting of or including decimal numbers.\n"
+     + "This includes lists of decimals, converted fractions, averages, etc.\n";
+     
+     System.out.println(doubletext);
+     
+     String booleantext = "A boolean data type represents one bit of information.\n"
+     + "There are only two possible boolean values: true or false.\n"
+     + "The boolean data type can be used for things that track true/false conditions.\n"
+     + "As it binary, it can be related to raw data as well, and can be used to decide whether to execute certain code or not.\n";
+     
+     System.out.println(booleantext);
+     
+     String chartext = "A char data type is a single character, meaning that each unit of data equals exactly one character.\n"
+     + "Char is used to store any character. This means it can be used in lists, to represent individual letters, abbreviations, etc.";
+     
+     System.out.println(chartext);
      
     } 
     
