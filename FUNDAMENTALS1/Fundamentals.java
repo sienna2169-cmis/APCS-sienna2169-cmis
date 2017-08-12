bc1da8e8ba88cf82fdc30e9a0335c04692ed8113
Sien <sienna2169@cmis.ac.th>
2017-08-12 16:42:21 +0700

    Finished Data Types, Variables, Static Methods problems :)

--- FUNDAMENTALS1/Fundamentals.java
+++ FUNDAMENTALS1/Fundamentals.java
@@ -5,26 +5,82 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.Scanner;
 public class Fundamentals
-{
+{ 
+ 
+    
   public static void main(String[] args) {
+      
+     
+     
+     
+      
      int c2 = 4;
      int c3 = 7;
      int c4 = 12;
-     int c5 = 32;
-     int c6 = 9;
-     int c7 = 18;
-     int c8 = 55;
+     int c5 = 20;
+     double c6 = 32.4;
+     double c7 = 9.3;
+     double c8 = 18.4;
+     double c9 = 55.2;
      
      // these are for the addition etc. methods
     dataTypes();
-    add(c2, c3);
-    subtract(30, 5);
-    multiply(8, 7);
-    doubleadd(3.6, 7.4);
-    doublesubtract(27.9, 98.5);
-    doublemultiply(3.5, 1.5);
-    doubledivide(4.763, 2);
+     // here's my dataTypes method
+     
+   Scanner scan = new Scanner(System.in);
+     System.out.println("Input an integer to subtract:");
+     int s = scan.nextInt();
+     System.out.println("Input another integer to subtract:");
+     int s2 = scan.nextInt();
+     System.out.println("Input an integer to divide:");
+     int s3 = scan.nextInt();
+     System.out.println("Input an integer to divide by:");
+     int s4 = scan.nextInt();
+     
+     System.out.println("Now input a decimal for subtraction:");
+     double s5 = scan.nextDouble();
+     
+     System.out.println("Input another decimal to subtract:");
+     double s6 = scan.nextDouble();
+     
+     System.out.println("Input a decimal to divide:");
+     double s7 = scan.nextDouble();
+     
+     System.out.println("Input another decimal to divide by:");
+     double s8 = scan.nextDouble();
+     
+     // NOTE: 'op' stands for operation
+     
+    int op1 = add(c2, c3);
+    int op2 = multiply(c4, c5);
+    // ^^ hard-coded variables 
+    int op3 = subtract(s, s2);
+    int op4 = divide(s3, s4);
+    // ^^ user input 
+    double op5 = doubleadd(c6, c7);
+    double op6 = doublemultiply(c8, c9); 
+    // ^^ hard-coded variables
+    double op7 = doublesubtract(s5, s6);
+    double op8 = doubledivide(s7, s8);
+    // ^^ user input
+    
+    System.out.println("Integer sum: " + op1);
+    System.out.println("Integer product: " + op2);
+    System.out.println("Integer difference: " + op3);
+    System.out.println("Integer quotient: " + op4);
+    // ^^ here's all the results for the integer methods
+    
+    System.out.println("Double sum: " + op5);
+    System.out.println("Double product: " + op6);
+    System.out.println("Double difference: " + op7);
+    System.out.println("Double quotient: " + op8);
+    // ^^ and here's the double method results
+    
+    
+   
+    
     
     }
   public static void dataTypes() {
@@ -103,7 +159,7 @@ public class Fundamentals
      System.out.println(booleantext);
      
      String chartext = "A char data type is a single character, meaning that each unit of data equals exactly one character.\n"
-     + "Char is used to store any character. This means it can be used in lists, to represent individual letters, abbreviations, etc.";
+     + "Char is used to store any character. This means it can be used in lists, to represent individual letters, abbreviations, etc.\n";
      
      System.out.println(chartext);
      //WOOOOOO I'M DONE!!!
@@ -112,7 +168,6 @@ public class Fundamentals
     int intone = a;
     int inttwo = b;
     int sum = intone + inttwo;
-    System.out.println(sum);
     return sum;
   
  }   
@@ -120,49 +175,42 @@ public class Fundamentals
     int intone = a;
     int inttwo = b;
     int difference = intone - inttwo;
-    System.out.println(difference);
     return difference;
  }  
     public static int multiply(int a, int b) {
     int intone = a;
     int inttwo = b;
     int product = intone * inttwo;
-    System.out.println(product);
     return product;
  }  
     public static int divide(int a, int b) {
     int intone = a;
     int inttwo = b;
     int quotient = intone / inttwo;
-    System.out.println(quotient);
     return quotient;
  }  
     public static double doubleadd(double a, double b) {
     double intone = a;
     double inttwo = b;
     double sum = intone + inttwo;
-    System.out.println(sum);
     return sum;
  }  
    public static double doublesubtract(double a, double b) {
     double intone = a;
     double inttwo = b;
     double difference = intone - inttwo;
-    System.out.println(difference);
     return difference;
  }  
   public static double doublemultiply(double a, double b) {
     double intone = a;
     double inttwo = b;
     double product = intone * inttwo;
-    System.out.println(product);
     return product;
  }  
   public static double doubledivide(double a, double b) {
     double intone = a;
     double inttwo = b;
     double quotient = intone / inttwo;
-    System.out.println(quotient);
     return quotient;
  }  
 }
