1912ba200d05a5c5c2d5e6efef55a0c2fba5f937
cmis <cmis@cmis-hs08.(none)>
2017-08-31 11:55:30 +0700

    Started Fundamentals 2

---
+++ FUNDAMENTALS2/Fundamentals2.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Fundamentals2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fundamentals2
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Fundamentals2
+     */
+    public Fundamentals2()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
