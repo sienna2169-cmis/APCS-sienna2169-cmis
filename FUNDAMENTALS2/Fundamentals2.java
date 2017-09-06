65d736a7680b16c8f7aba07545caeebc33f7e054
cmis <cmis@cmis-hs08.(none)>
2017-09-06 11:14:48 +0700

    Fundamentals 2 work

--- FUNDAMENTALS2/Fundamentals2.java
+++ FUNDAMENTALS2/Fundamentals2.java
@@ -24,49 +24,148 @@ public class Fundamentals2
     c[3] = "an";
     c[4] = "array!";
     
-    System.out.println(a[0]);
-    System.out.println(b[2]);
-    System.out.println(c[4]);
+    //System.out.println(a[0]);
+    //System.out.println(b[2]);
+    //System.out.println(c[4]);
     
     printArray(b, true);
     printArray(c, false);
-    
+    System.out.println(lastItem(a));
     }
    public static void printArray(int a[], boolean b) {
-       if (b = true) {
-        for (int i = 1; i < a.length ; i++) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
         System.out.println(a[i]);
         }
         } 
-       else {
-         for (int i = 1; i < a.length ; i++) {
+       if (b == false) {
+       for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
         System.out.println(a[i]);
         }
-        }
-    }
+        if(i%2 != 0) {continue;}
+       }
+        } }
    public static void printArray(double a[], boolean b) {
-       if (b = true) {
-        for (int i = 1; i < a.length ; i++) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
         System.out.println(a[i]);
         }
         } 
-       else {
-         for (int i = 1; i < a.length ; i++) {
+       if(b == false) {
+       for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
         System.out.println(a[i]);
         }
-        }
-                }
-   
-   public static void printArray( String a[], boolean b) {
-        if (b = true) {
-        for (int i = 1; i < a.length ; i++) {
+        if(i%2 != 0) {continue;}
+       }
+        }}
+   public static void printArray(String a[], boolean b) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
         System.out.println(a[i]);
         }
         } 
-       else {
-         for (int i = 1; i < a.length ; i++) {
+       if(b == false) {
+         for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
         System.out.println(a[i]);
         }
+        if(i%2 != 0) {continue;}
+       }
+        }}
+   public static void printArray(char a[], boolean b) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
+        System.out.println(a[i]);
         }
+        } 
+       if(b == false) {
+         for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
+        System.out.println(a[i]);
+        }
+        if(i%2 != 0) {continue;}
+       }
+        }}
+   public static void printArray(byte a[], boolean b) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
+        System.out.println(a[i]);
+        }
+        } 
+       if(b == false) {
+         for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
+        System.out.println(a[i]);
+        }
+        if(i%2 != 0) {continue;}
+       }
+        }}
+   public static void printArray(short a[], boolean b) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
+        System.out.println(a[i]);
+        }
+        } 
+       if (b == false) {
+         for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
+        System.out.println(a[i]);
+        }
+        if(i%2 != 0) {continue;}
+       }
+        }}
+   public static void printArray(long a[], boolean b) {
+       if (b == true) {
+        for (int i = 0; i < a.length ; i++) {
+        System.out.println(a[i]);
+        }
+        } 
+       if (b== false) {
+         for (int i = 0; i < a.length ; i++) {
+        if(i % 2 == 0) {
+        System.out.println(a[i]);
+        }
+        if(i%2 != 0) {continue;}
+       }
+        }}
+        
+   public static int lastItem(int a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }
+   public static double lastItem(double a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }
+   public static String lastItem(String a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }     
+   public static char lastItem(char a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }
+   public static short lastItem(short a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }
+   public static long lastItem(long a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }
+   public static byte lastItem(byte a[]) {
+        int length = a.length;
+        return a[length-1];
+        
+        }     
+ 
     }
-}
+
