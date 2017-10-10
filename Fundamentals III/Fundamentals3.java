ac5a4330cb66e79cc3bdcf17db55371240587b0a
cmis <cmis@cmis-hs08.(none)>
2017-10-10 09:55:36 +0700

    started fundamentals III

---
+++ Fundamentals III/Fundamentals3.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Fundamentals3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fundamentals3
+{ public static void main(String[] args) {
+        int[][] array1 = new int[5][];
+
+        int[][] array2 =    
+                {{0, 2, 6, 1, 1},
+                {3, 7, 8, 4, 7},
+                {1, 1, 9, 0, 2},
+                {2, 4, 2, 7, 2},
+                {7, 5, 9, 4, 3}};             
+    }
+}
