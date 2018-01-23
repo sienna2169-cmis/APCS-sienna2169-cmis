5340c5b7be3eca533767f6c25183468c74dbbb32
sienna <sienna2169@cmis.ac.th>
2018-01-23 13:29:33 +0700

    hurgh

---
+++ OOp/Crab.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Crab here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Crab
+{
+    private String name;
+    private int age;
+    private double size;
+    private boolean isMale;
+    }
+
---
+++ OOp/Program.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Program here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Program
+{   public static void main(String[] args) {
+    Crab b = new Crab();
+    
+    
+}
+}
