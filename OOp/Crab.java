ad01b1bd7fd93d3efcc2d92f0f012fa6ff0b2780
sienna <sienna2169@cmis.ac.th>
2018-01-23 14:01:09 +0700

    oop

--- OOp/Crab.java
+++ OOp/Crab.java
@@ -7,9 +7,32 @@
  */
 public class Crab
 {
-    private String name;
-    private int age;
-    private double size;
-    private boolean isMale;
+   private String name;
+   private int age;
+   private double size;
+   private boolean isMale;
+   
+   public Crab(String _name){
+    this.name = _name;
     }
-
+   public Crab(boolean gender) {
+    this.isMale = gender;
+    }
+   public Crab(double _size) {
+    if (this.size < 0) {
+    _size = 0.0;
+    }
+    else {
+    this.size = _size;
+    }
+    
+    }
+    public String toString(){
+    String out = "";
+    out += "name:" + name + "\n";
+    out += "Age:" + age + "\n";
+    out += "Height:" + size + "\n";
+    return out;
+    
+    }
+}
--- OOp/Program.java
+++ OOp/Program.java
@@ -6,9 +6,13 @@
  * @version (a version number or a date)
  */
 public class Program
-{   public static void main(String[] args) {
-    Crab b = new Crab();
+{
+   public static void main(String[] args) {
+    Crab b = new Crab("joel");
+    
+    System.out.println(b);
     
     
-}
+    
+    }
 }
