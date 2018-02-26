885f35289cd4266c48eca7f9c0e3b966da30d54e
Sienna <sienna2169@cmis.ac.th>
2018-02-26 11:53:35 +0700

    working on ClockStore

---
+++ OOp/ClockStore.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class ClockStore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ClockStore
+{
+   private Clock[] clocksInStock = new Clock[3];
+   
+   public static void main(String[] args) {
+    ClockStore store1 = new ClockStore();
+    store1.clocksInStock[0] = new Clock(11, 34, 05);
+    store1.clocksInStock[1] = new Clock(02, 44, 17);
+    store1.clocksInStock[2] = new Clock(13,05, 51);
+    
+    
+    }
+    int mostSeconds(){
+    
+    return 1;
+    }
+    }
+
